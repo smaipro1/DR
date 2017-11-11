a660bc7e5619a53227b807acc2675fca03884fd4
smaipro1 <abhilash.sristi@gmail.com>
2017-11-11 22:35:16 +0530

    pca compplete hopefuly

--- PCA/pca.py
+++ PCA/pca.py
@@ -1,28 +1,30 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-#this is pca code
+
+
 sample_length=784
 red_sample_length=sample_length/4
-
-#filepath_or_buffer='~/DATASET/mnist_test.csv',
 df = pd.read_csv(
-    filepath_or_buffer='~/DATASET/testdata.csv', 
+    filepath_or_buffer='~/DATASET/dataset.csv', 
     header=None, 
     sep=',')
 
-df.columns=range(0,sample_length+1)
-df.dropna(how="all", inplace=True) # drops the empty line at file-end
-df.tail()
-
-# split data table into data X and class labels y
-
 X = df.ix[:,1:sample_length].values
-y = df.ix[:,0].values
-
-X_std=X
-u,s,v = np.linalg.svd(X_std.T)
+print X.shape
+u,s,v = np.linalg.svd(X.T)
 red = u[0:sample_length/4,:]
-X_red = np.dot(X_std,red.T)
-for i in X_red:
-    print i
+X_red = np.dot(X,red.T)
+# for i in X_red:
+    # print i
+print X_red.shape
+
+matrix = X[0].reshape((28,28))
+plt.imshow(matrix,cmap='gray') 
+plt.savefig('HELLO1')
+
+
+matrix_red = X_red[0].reshape((14, 14))
+plt.imshow(matrix_red,cmap='gray') 
+plt.savefig('HELLO2')
