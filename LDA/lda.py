57cb722e5fd356f8793cb52711bb7d89be720f08
smaipro1 <abhilash.sristi@gmail.com>
2017-11-26 21:13:10 +0530

    mds fix

--- LDA/lda.py
+++ LDA/lda.py
@@ -1,13 +1,30 @@
 import numpy as np
-#import pandas as pd
+import sys
 import matplotlib.pyplot as plt
+sys.path.append('../DATA_READ/')
+from read_data import *
+import math
+[X,Y]=read_dataset(sys.argv[1])
 
-sample_length=784
-red_sample_length=sample_length/4
-df = pd.read_csv(
-    filepath_or_buffer='~/DATASET/dataset.csv', 
-    header=None, 
-    sep=',')
-    
-X = df.ix[:,1:sample_length].values
-print X.shape
+s = (784,784)
+
+np.set_printoptions(precision=10)
+
+mean_vectors = []
+for cl in range(0,10):
+    mean_vectors.append(np.mean(X[Y==cl], axis=0))
+    #print('Mean Vector class %s: %s\n' %(cl, mean_vectors[cl-1]))
+
+
+
+S_W = np.zeros(s)
+for cl,mv in zip(range(0,10), mean_vectors):
+    class_sc_mat = np.zeros(s)                  # scatter matrix for every class
+    for row in X[Y == cl]:
+        row, mv = row.reshape(sample_length,1), mv.reshape(sample_length,1) # make column vectors
+        class_sc_mat += np.dot((row-mv),(row-mv).T)
+    S_W += class_sc_mat                             # sum class scatter matrices
+print('within-class Scatter Matrix:\n', S_W)
+# for i in S_W:
+	# print i
+print np.linalg.det(S_W)
--- MDS/mds.py
+++ MDS/mds.py
@@ -4,21 +4,20 @@ from numpy.linalg import *
 from numpy.random import *
 import pylab as plt
 
-def main():
+sys.path.append('../DATA_READ/')
+from read_data import * 
+[X,Y]=read_dataset(sys.argv[1])
+points=X
+labels=Y
 
-    points = loadtxt("mnist2500_X.txt");
-    labels = loadtxt("mnist2500_labels.txt");
 
-    (n, d) = points.shape;
-    points = points - tile(mean(points, 0), (n, 1));
-    (l, M) = linalg.eig(dot(points.T, points));
-    X = dot(points, M[:,0:2]);
-    (n, d) = X.shape;
+#points = loadtxt("mnist2500_X.txt");
+#labels = loadtxt("mnist2500_labels.txt");
 
-    plt.scatter(X[:,0], X[:,1], 20, labels);
-
-    plt.show()
-
-if __name__ == "__main__":
-    main()
-import pylab as plt
+(n, d) = points.shape;
+points = points - tile(mean(points, 0), (n, 1));
+(l, M) = linalg.eig(dot(points.T, points));
+X = dot(points, M[:,0:2]);
+(n, d) = X.shape;
+plt.scatter(X[:,0], X[:,1], 20, labels);
+plt.show()
